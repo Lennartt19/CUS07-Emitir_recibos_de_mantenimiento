51c9cb9094333e071d6583022c4f4452f300af97
cesarahp02 <cesar_ahp@outlook.com>
2023-11-17 09:07:29 -0500

    Funcionalidad Botones Registrar Casa + Modificaciones Visuales

--- Frontend/CONDOSA/src/app/funciones-principales/registrar-gasto-casa/registrar-gasto-casa.component.ts
+++ Frontend/CONDOSA/src/app/funciones-principales/registrar-gasto-casa/registrar-gasto-casa.component.ts
@@ -1,28 +1,26 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Casas } from 'src/app/models/casas';
 
-
 @Component({
   selector: 'app-registrar-gasto-casa',
   templateUrl: './registrar-gasto-casa.component.html',
   styleUrls: ['./registrar-gasto-casa.component.css']
 })
 export class RegistrarGastoCasaComponent implements OnInit {
-
-  @Output() mostrarRegistroCasa_OUT=new EventEmitter<boolean>();
-  @Output() idCasaRegistro_OUT=new EventEmitter<string>();
+  @Output() mostrarRegistroCasa_OUT = new EventEmitter<boolean>();
+  @Output() idCasaRegistro_OUT = new EventEmitter<string>();
   @Input() casasArray_IN: Casas[] = [];
-  //DATOS DE LA CASA
-  num_casa_selected:string='--N° Casa--';
-  id_casa_selected:string='';
-  
-  
 
+  // DATOS DE LA CASA
+  num_casa_selected: string = '--N° Casa--';
+  id_casa_selected: string = '';
 
+  montoEditable: number | null = null;
+  montoSeleccionado: number | null = null; // Nueva propiedad para almacenar el monto seleccionado
 
-  listaTipoGastoCasas: any[] = ['Gasto genrico', '´Sanciones'];
+  listaTipoGastoCasas: any[] = ['Gasto generico', 'Sanciones'];
   listaDescripcionCasas: any[] = ['Agua', 'Luz y Electricidad'];
-  
+
   gatosRegistradosCasa: any[] = [
     { TipoGasto: 'Gasto de agua individual', Monto: 30},
     { TipoGasto: 'Administración y contabilidad ', Monto: 66 },
@@ -51,7 +49,6 @@ export class RegistrarGastoCasaComponent implements OnInit {
     { TipoGasto: 'Administración y contabilidad ', Monto: 660 },
     { TipoGasto: 'Teléfono fijo e internet',  Monto: 123},
     { TipoGasto: 'Consumo de Luz Mensual SS-GG -Suministro 1695605',  Monto: 123 },
-    { TipoGasto: 'Consumo de Luz Mensual BCI -Suministro 1695613',  Monto: 123},
     { TipoGasto: 'Planilla (portería- áreas comunes- limpieza)', Monto: 305},
     { TipoGasto: 'Administración y contabilidad ', Monto: 660 },
     { TipoGasto: 'Teléfono fijo e internet',  Monto: 123},
@@ -60,21 +57,60 @@ export class RegistrarGastoCasaComponent implements OnInit {
   ];
 
   ngOnInit() {
+    // Recuperar datos almacenados localmente al cargar el componente
+    const storedData = localStorage.getItem('gastosRegistradosCasa');
+    if (storedData) {
+      this.gatosRegistradosCasa = JSON.parse(storedData);
+    }
   }
 
-  set_mostrarRegistroCasa(item:boolean){
+  set_mostrarRegistroCasa(item: boolean) {
     this.mostrarRegistroCasa_OUT.emit(item);
   }
-  
-  finalizarRegistroCasa(){
-    //Aca iria el metodo para modificar la tabla ESTADO_REGISTRO_PREDIO de la BD
-    if(this.num_casa_selected !== '--N° Casa--'){
-      console.log("se manda el numero:"+ this.num_casa_selected)
-      this.idCasaRegistro_OUT.emit(this.id_casa_selected);
-    }else{
-      alert('Selecciona un numero de casa');
+
+  registrarGasto() {
+    // Verifica que se haya seleccionado una descripción y un número de casa
+    if (this.num_casa_selected === '--N° Casa--' || !this.listaDescripcionCasas || this.listaDescripcionCasas.length === 0) {
+      alert('Por favor, seleccione una descripción antes de registrar el gasto.');
+      return;
     }
-    
-  }
   
+    // Obtén el monto correspondiente al tipo de gasto seleccionado
+    let tipoGastoSeleccionado: string;
+  
+    if (this.listaDescripcionCasas.includes('Agua')) {
+      tipoGastoSeleccionado = 'Gasto de agua individual';
+    } else if (this.listaDescripcionCasas.includes('Luz y Electricidad')) {
+      tipoGastoSeleccionado = 'Consumo de Luz Mensual SS-GG -Suministro 1695605';
+    } else {
+      // Manejo de caso por defecto si no coincide con ninguna descripción conocida
+      console.error('Descripción no reconocida:', this.listaDescripcionCasas);
+      return;
+    }
+  
+    const gastoEncontrado = this.gatosRegistradosCasa.find(gasto => gasto.TipoGasto === tipoGastoSeleccionado);
+  
+    // Asigna el monto encontrado a la propiedad montoSeleccionado
+    this.montoSeleccionado = gastoEncontrado ? gastoEncontrado.Monto : null;
+  }
+
+  finalizarRegistroCasa() {
+    // Aca iría el método para modificar la tabla ESTADO_REGISTRO_PREDIO de la BD
+    localStorage.setItem('gastosRegistradosCasa', JSON.stringify(this.gatosRegistradosCasa));
+    alert('Se han actualizado correctamente los gastos');
+    window.location.reload();
+  }
+
+  editarMonto(gasto: any) {
+    if (this.montoEditable === null) {
+      // Si no se está editando ningún monto, permite la edición para este gasto
+      this.montoEditable = gasto.Monto;
+      this.montoSeleccionado = gasto.Monto; // Agregamos esta línea para almacenar el monto original
+    } else {
+      // Si se está editando algún monto, guarda los cambios y limpia la variable editable
+      gasto.Monto = this.montoEditable;
+      this.montoEditable = null;
+      this.montoSeleccionado = null; // Limpiamos la variable montoSeleccionado
+    }
+  }
 }
